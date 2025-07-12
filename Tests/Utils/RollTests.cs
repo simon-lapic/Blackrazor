3a5e00e4f0b3e583798b8e6c129ae0e8ec731c2f
Simon Lapic <simonlapic@gmail.com>
2025-07-12 16:27:46 -0400

    updated roll tests to output results for manual confirmation

---
+++ Tests/Utils/RollTests.cs
@@ -0,0 +1,43 @@
+﻿using Blackrazor.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Blackrazor.Tests.Utils
+{
+    public class RollTests
+    {
+        public static IEnumerable<object[]> RollStrings = new List<object[]>
+        {
+            new object[] { "1d20" },
+            new object[] { "2d20" },
+            new object[] { "+1d20" },
+            new object[] { "-1d20" },
+            new object[] { "1d20+1" },
+            new object[] { "1d20-1" },
+            new object[] { "1d20+1d10" },
+            new object[] { "1d20-1d10" },
+            new object[] { "1d20+1d10+1" },
+            new object[] { "3d20+4d10+10-4" },
+        };
+
+        private readonly ITestOutputHelper Output;
+
+        public RollTests(ITestOutputHelper output)
+        {
+            this.Output = output;
+        }
+
+        [Theory(DisplayName = "Constructor")]
+        [MemberData(nameof(RollStrings))]
+        public void RollConstructor(string constructorArg)
+        {
+            Dice dice = new Dice(constructorArg);
+            dice.Roll();
+            Output.WriteLine($"{dice} => {dice.Result}");
+        }
+    }
+}
