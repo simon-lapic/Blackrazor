866e92276b35d6d710806feaf18fd056ba901784
Simon Lapic <simonlapic@gmail.com>
2025-08-03 12:11:27 -0400

    removed unnecessary method from interface

---
+++ Blackrazor/Dice/IRandomnessProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Blackrazor.Dice
+{
+    public interface IRandomnessProvider
+    {
+        /// <summary>
+        /// Gets a random number in the range <c>[0, <paramref name="maximum"/>)</c>
+        /// </summary>
+        /// <param name="maximum"></param>
+        /// <returns></returns>
+        public int GetRandomNumber(int maximum);
+    }
+}
