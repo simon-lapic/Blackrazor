2dbae113839574253898bd0b357bc46729892425
Simon Lapic <simonlapic@gmail.com>
2025-08-02 21:58:42 -0400

    updated interface

---
+++ Blackrazor/Dice/IDieRoll.cs
@@ -0,0 +1,43 @@
+﻿namespace Blackrazor.Utils
+{
+    /// <summary>
+    /// An interface that can be implemented to represent an object that 
+    /// can store, roll, and reroll a set of dice
+    /// </summary>
+    public interface IDieRoll
+    {
+        /// <summary>
+        /// True if the die roll value is negated when calculated 
+        /// (multiplied by <c>-1</c>) when calculated, otherwise false
+        /// </summary>
+        public bool IsNegative { get; }
+
+        /// <summary>
+        /// A string representation of the dice
+        /// </summary>
+        public string DiceString { get; }
+
+        /// <summary>
+        /// The last result of rolling the dice
+        /// </summary>
+        public int Value { get; }
+
+        /// <summary>
+        /// A string representation of the last result of rolling the dice
+        /// </summary>
+        public string ValueString { get; }
+
+        /// <summary>
+        /// Rolls the dice
+        /// </summary>
+        /// <returns></returns>
+        public int Roll();
+
+        /// <summary>
+        /// Sets the value of each die in the die roll 
+        /// </summary>
+        /// <param name="dieValues"></param>
+        /// <returns></returns>
+        public int SetRoll(params int[] dieValues);
+    }
+}
