e49f501024e838511a1e4ec3e3b70d2f1da7687b
Simon Lapic <simonlapic@gmail.com>
2025-08-03 12:14:18 -0400

    implemented DiceCollection for more complex dice rolls

---
+++ Blackrazor/Dice/DiceCollection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Blackrazor.Dice
+{
+    public class DiceCollection : IDiceRoll, IEnumerable<IDiceRoll>
+    {
+        public bool IsNegative { get; }
+
+        public string DiceString { get; private set; }
+
+        public IDiceRoll[] Dice { get; }
+
+        public int Result { get; private set; }
+
+        public string ResultString { get; private set; }
+
+        public DiceCollection(IEnumerable<IDiceRoll> dice, bool isNegative = false)
+        {
+            Dice = [.. dice];
+            IsNegative = isNegative;
+
+            DiceString = ToString();
+            ResultString = "";
+            Result = Roll();
+        }
+
+        public int Roll()
+        {
+            if (Dice.Length == 0)
+                return 0;
+
+            int result = Dice[0].Roll(out string resultString);
+            for (int i = 1; i < Dice.Length; i++)
+            {
+                if (Dice[i].IsNegative)
+                {
+                    result += Dice[i].Roll();
+                    resultString += " - " + Dice[i].ResultString.TrimStart('-');
+                }
+                else
+                {
+                    result += Dice[i].Roll();
+                    resultString += " + " + Dice[i].ResultString;
+                }
+            }
+
+            ResultString = IsNegative ? $"-({resultString})" : resultString;
+            return result * (IsNegative ? -1 : 1);
+        }
+
+        public int Roll(out string rollString)
+        {
+            int result = Roll();
+            rollString = ResultString;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            if (!string.IsNullOrEmpty(DiceString))
+                return DiceString;
+
+            if (Dice.Length == 0)
+                return "";
+
+            string diceString = Dice[0].DiceString;
+            for (int i = 1; i < Dice.Length; i++)
+            {
+                if (Dice[i].IsNegative)
+                    diceString += " - " + Dice[i].DiceString.TrimStart('-');
+                else
+                    diceString += " + " + Dice[i].DiceString;
+            }
+            return diceString;
+        }
+
+        public string ToString(bool includeResult)
+        {
+            if (!includeResult)
+                return ToString();
+            return ResultString + $" = {Result}";
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+            => ToString(format == "r" || format == "res" || format == "result");
+
+        public IEnumerator<IDiceRoll> GetEnumerator()
+        {
+            return new DiceEnumerator(Dice);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Blackrazor/Dice/DiceEnumerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Blackrazor.Dice
+{
+    public class DiceEnumerator(IDiceRoll[] dice) : IEnumerator<IDiceRoll>
+    {
+        /// <summary>
+        /// The array of <see cref="IDiceRoll"/> used to manage the position internally
+        /// </summary>
+        private IDiceRoll[] _dice = dice;
+
+        /// <summary>
+        /// The position of the <see cref="Current"/> <see cref="IDiceRoll"/>
+        /// </summary>
+        private int _position = -1;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public IDiceRoll Current
+        {
+            get
+            {
+                try
+                {
+                    return _dice[_position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        object IEnumerator.Current => Current;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public void Dispose() { }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public bool MoveNext() => ++_position < _dice.Length;
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public void Reset() => _position = -1;
+    }
+}
