4bd64164e525cd756027e7ee41d4640ba31e3e33
Simon Lapic <simonlapic@gmail.com>
2025-08-03 12:12:54 -0400

    added interface for a dice string parser

---
+++ Blackrazor/Dice/IDiceStringParser.cs
@@ -0,0 +1,12 @@
+﻿namespace Blackrazor.Dice
+{
+    public interface IDiceStringParser
+    {
+        /// <summary>
+        /// Gets an <see cref="IDiceRoll"/> that represents the provided <paramref name="diceString"/>
+        /// </summary>
+        /// <param name="diceString"></param>
+        /// <returns></returns>
+        public IDiceRoll GetDice(string diceString);
+    }
+}
